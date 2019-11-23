fd2a8ec43d3d93baab71845449ab1ecfbaaf0906
amb seny <adrifre7@hotmail.com>
2019-11-23 08:46:06 +0100

    sobrasada code

---
+++ SobrasadaShop/SobrasadaShop.TextConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SobrasadaShop.TextConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sobrasada = new Sobrasada();
+            Console.WriteLine("Your sobrasada costs " + sobrasada.Price);
+        }
+    }
+}
---
+++ SobrasadaShop/SobrasadaShop.TextConsole/Sobrasada.cs
@@ -0,0 +1,11 @@
+﻿namespace SobrasadaShop.TextConsole
+{
+    public class Sobrasada
+    {
+        private float basePrice = 6.0f;
+        public float Price
+        {
+            get => basePrice * 1.16f;
+        }
+    }
+}
